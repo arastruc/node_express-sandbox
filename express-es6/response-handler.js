8e1d13eddb2119a853ca8958f3a9ef528d5dd2ab
aastruc <aastruc86@gmail.com>
2024-04-05 15:45:08 +0200

    fix : clear unused codes

--- express-es6/response-handler.js
+++ express-es6/response-handler.js
@@ -5,17 +5,10 @@ import { join, dirname } from "path";
 const resHandler = (_, res, next) => {
   const __dirname = dirname(import.meta.filename);
 
-  ////   Classic Way
-  //   fs.readFile(join(__dirname, "my-page.html"), "utf8", (err, data) => {
-  //     res.send(data);
-  //   });
-
   //   Promises Way
   fs.readFile(join(__dirname, "my-page.html"), "utf8")
     .then((data) => res.send(data))
     .catch(console.log);
-
-  //   res.sendFile(join(__dirname, "my-page.html"));
 };
 
 export default resHandler;
